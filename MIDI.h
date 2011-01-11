72f358dea7fe51d9e2da57024e0062a7c5222b4c
Florian Ragwitz <rafl@debian.org>
2011-01-11 23:47:09 +0100

    Set up LEDs for the arduino

--- MIDI.h
+++ MIDI.h
@@ -52,13 +52,13 @@
 
     /* Macros: */
         /** LED mask for the library LED driver, to indicate that the USB interface is not ready. */
-        #define LEDMASK_USB_NOTREADY      LEDS_LED1
+        #define LEDMASK_USB_NOTREADY      LEDS_NO_LEDS
 
         /** LED mask for the library LED driver, to indicate that the USB interface is enumerating. */
-        #define LEDMASK_USB_ENUMERATING  (LEDS_LED2 | LEDS_LED3)
+        #define LEDMASK_USB_ENUMERATING  LEDS_LED2
 
         /** LED mask for the library LED driver, to indicate that the USB interface is ready. */
-        #define LEDMASK_USB_READY        (LEDS_LED2 | LEDS_LED4)
+        #define LEDMASK_USB_READY        LEDS_LED1
 
         /** LED mask for the library LED driver, to indicate that an error has occurred in the USB interface. */
         #define LEDMASK_USB_ERROR        (LEDS_LED1 | LEDS_LED3)
